ee26a8d0605e6383748c30e37614f828bc390589
Ben Hale <bhale@vmware.com>
2020-10-06 13:24:19 -0700

    BellSoft Liberica Dependency Action
    This change contributes a BellSoft Liberica Dependency action that selects the
    latest version of BellSoft Liberica JVMs.
    
    Signed-off-by: Ben Hale <bhale@vmware.com>

---
+++ actions/bellsoft-liberica-dependency/main.go
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2018-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/paketo-buildpacks/pipeline-builder/actions"
+)
+
+func main() {
+	inputs := actions.NewInputs()
+
+	t, ok := inputs["type"]
+	if !ok {
+		panic(fmt.Errorf("type must be specified"))
+	}
+
+	v, ok := inputs["version"]
+	if !ok {
+		panic(fmt.Errorf("version must be specified"))
+	}
+
+	uri := fmt.Sprintf("https://api.bell-sw.com/v1/liberica/releases"+
+		"?arch=x86"+
+		"&bitness=64"+
+		"&bundle-type=%s"+
+		"&os=linux"+
+		"&package-type=tar.gz"+
+		"&version-feature=%s"+
+		"&version-modifier=latest",
+		t, v)
+
+	resp, err := http.Get(uri)
+	if err != nil {
+		panic(fmt.Errorf("unable to get %s\n%w", uri, err))
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		panic(fmt.Errorf("unable to download %s: %d", uri, resp.StatusCode))
+	}
+
+	var raw []Release
+	if err := json.NewDecoder(resp.Body).Decode(&raw); err != nil {
+		panic(fmt.Errorf("unable to decode payload\n%w", err))
+	}
+
+	versions := make(actions.Versions)
+
+	for _, r := range raw {
+		versions[fmt.Sprintf("%d.%d.%d+%d", r.FeatureVersion, r.InterimVersion, r.UpdateVersion, r.BuildVersion)] = r.DownloadURL
+	}
+
+	versions.GetLatest().Write(os.Stdout)
+}
+
+type Release struct {
+	FeatureVersion int    `json:"featureVersion"`
+	InterimVersion int    `json:"interimVersion"`
+	UpdateVersion  int    `json:"updateVersion"`
+	BuildVersion   int    `json:"buildVersion"`
+	DownloadURL    string `json:"downloadUrl"`
+}

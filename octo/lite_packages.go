5946d89e3f44158016a771dfdfac47574a186830
Daniel Mikusa <dmikusa@vmware.com>
2021-11-08 14:51:02 -0500

    Adds a pipeline generator to create republish-image workflows
    - A republish-image workflow is used to republish an existing buildpack image under a different buildpack id
    - This can be used to take a paketo-buildpacks buildpack and republish it under a different buildpack id
    - The advantage of this method is that it doesn't require the buildpack to be rebuilt and packaged, it takes the existing image, modifies it and then creates a new image with the included modifications. It does not change the source image.
    - These workflows are configured to run based on a CRON schedule such that it executes at 15 minutes past the hour, between 12:00 PM and 11:59 PM UTC, Monday through Friday.
    - The job checks for new versions by looking at tags on the source image and comparing to tags on the target image. It iterates over the tags on source in reverse sorted order (highest first) and checks if that exists on target image. If it exists, it outputs information to skip doing any additional work. If it is not present it indicates that this version should be repackaged.
    
    Signed-off-by: Daniel Mikusa <dmikusa@vmware.com>

---
+++ octo/lite_packages.go
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2018-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package octo
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/paketo-buildpacks/pipeline-builder/octo/actions"
+	"github.com/paketo-buildpacks/pipeline-builder/octo/actions/event"
+)
+
+func ContributeLitePackages(descriptor Descriptor) ([]Contribution, error) {
+	var contributions []Contribution
+
+	for _, o := range descriptor.RepublishImages {
+		if c, err := contributeLitePackage(descriptor, o); err != nil {
+			return nil, err
+		} else {
+			contributions = append(contributions, c)
+		}
+	}
+
+	return contributions, nil
+}
+
+func contributeLitePackage(descriptor Descriptor, republishImage RepublishImage) (Contribution, error) {
+	w := actions.Workflow{
+		Name: fmt.Sprintf("Republish Image %s", filepath.Base(republishImage.Target)),
+		On: map[event.Type]event.Event{
+			event.ScheduleType:         event.Schedule{{Minute: "15", Hour: "12-23", DayOfWeek: "1-5"}},
+			event.WorkflowDispatchType: event.WorkflowDispatch{},
+		},
+		Jobs: map[string]actions.Job{
+			"republish-image": {
+				Name:   "Republish Image with new ID",
+				RunsOn: []actions.VirtualEnvironment{actions.UbuntuLatest},
+				Steps: []actions.Step{
+					{
+						Name: "Install crane",
+						Run:  StatikString("/install-crane.sh"),
+						Env:  map[string]string{"CRANE_VERSION": CraneVersion},
+					},
+					{
+						Id:   "version",
+						Name: "Check for next version",
+						Run:  StatikString("/check-republish-version.sh"),
+						Env: map[string]string{
+							"SOURCE": republishImage.Source,
+							"TARGET": republishImage.Target,
+						},
+					},
+					{
+						Uses: "actions/setup-go@v2",
+						If:   "${{ ! steps.version.outputs.skip }}",
+						With: map[string]interface{}{"go-version": GoVersion},
+					},
+					{
+						Name: "Install update-buildpack-image-id",
+						If:   "${{ ! steps.version.outputs.skip }}",
+						Run:  StatikString("/install-update-buildpack-image-id.sh"),
+					},
+					{
+						Name: "Republish Image",
+						If:   "${{ ! steps.version.outputs.skip }}",
+						Run:  StatikString("/republish-image.sh"),
+						Env: map[string]string{
+							"SOURCE":  republishImage.Source,
+							"TARGET":  republishImage.Target,
+							"VERSION": "${{ steps.version.outputs.version }}",
+							"NEWID":   republishImage.ID,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	j := w.Jobs["republish-image"]
+	j.Steps = append(NewDockerCredentialActions(descriptor.DockerCredentials), j.Steps...)
+	j.Steps = append(NewHttpCredentialActions(descriptor.HttpCredentials), j.Steps...)
+	w.Jobs["republish-image"] = j
+
+	return NewActionContribution(w)
+}

7b98415849f9f99275824d46e94b4e81025e6bac
Ozzy Osborne <bardweller@gmail.com>
2023-08-14 14:27:12 -0400

    swap from @latest to @v2.0.0-alpha.1 as default
    Signed-off-by: Ozzy Osborne <bardweller@gmail.com>

--- octo/install-update-lifecycle-dependency.sh
+++ octo/install-update-lifecycle-dependency.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-go install -ldflags="-s -w" github.com/paketo-buildpacks/libpak/v2/cmd/update-lifecycle-dependency@${PAKETO_LIBPAK_COMMIT:=latest}
+go install -ldflags="-s -w" github.com/paketo-buildpacks/libpak/v2/cmd/update-lifecycle-dependency@${PAKETO_LIBPAK_COMMIT:=v2.0.0-alpha.1}

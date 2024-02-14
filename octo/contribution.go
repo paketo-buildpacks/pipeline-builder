a6b115ef6ad24e565e83694bb96d0c3112dd2eeb
Daniel Mikusa <dan@mikusa.com>
2024-02-13 21:37:38 -0500

    Bump action versions
    Bump versions for:
    - docker/login-action
    - mheap/github-action-required-labels
    - peter-evans/create-pull-request
    
    Signed-off-by: Daniel Mikusa <dan@mikusa.com>

--- octo/contribution.go
+++ octo/contribution.go
@@ -105,7 +105,7 @@ func NewDockerCredentialActions(credentials []DockerCredentials) []actions.Step 
 		s = append(s, actions.Step{
 			Name: fmt.Sprintf("Docker login %s", c.Registry),
 			If:   "${{ (github.event_name != 'pull_request' || ! github.event.pull_request.head.repo.fork) && (github.actor != 'dependabot[bot]') }}",
-			Uses: "docker/login-action@v2",
+			Uses: "docker/login-action@v3",
 			With: map[string]interface{}{
 				"registry": c.Registry,
 				"username": c.Username,

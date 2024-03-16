a2620823b44d28246869225165acea3d6c6a2a5c
Daniel Mikusa <dan@mikusa.com>
2024-03-16 15:58:36 -0400

    Make go module dynamic in build script
    Signed-off-by: Daniel Mikusa <dan@mikusa.com>

--- octo/build-script.sh
+++ octo/build-script.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-GOOS="linux" go build -ldflags='-s -w' -o linux/amd64/bin/main github.com/paketo-buildpacks/procfile/v5/cmd/main
-GOOS="linux" GOARCH="arm64" go build -ldflags='-s -w' -o linux/arm64/bin/main github.com/paketo-buildpacks/procfile/v5/cmd/main
+GOMOD=$(head -1 go.mod | awk '{print $2}')
+
+GOOS="linux" go build -ldflags='-s -w' -o linux/amd64/bin/main "$GOMOD/cmd/main"
+GOOS="linux" GOARCH="arm64" go build -ldflags='-s -w' -o linux/arm64/bin/main "$GOMOD/cmd/main"
 
 if [ "${STRIP:-false}" != "false" ]; then
   strip linux/amd64/bin/main linux/arm64/bin/main

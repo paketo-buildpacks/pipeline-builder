a55acb995c9c0889ad4ecf589252ab67e142136b
Daniel Mikusa <dan@mikusa.com>
2024-03-17 23:41:07 -0400

    Add fix for strip/compress
    Signed-off-by: Daniel Mikusa <dan@mikusa.com>

--- octo/build-script.sh
+++ octo/build-script.sh
@@ -4,17 +4,23 @@ set -euo pipefail
 GOMOD=$(head -1 go.mod | awk '{print $2}')
 
 {{- range $key, $value := .}}
-GOOS="linux" go build -ldflags='-s -w' -o "{{ $key }}" "{{ $value }}"
-GOOS="linux" GOARCH="arm64" go build -ldflags='-s -w' -o "{{ $key }}" "{{ $value }}"
+GOOS="linux" go build -ldflags='-s -w' -o "linux/amd64/{{ $key }}" "{{ $value }}"
+GOOS="linux" GOARCH="arm64" go build -ldflags='-s -w' -o "linux/arm64/{{ $key }}" "{{ $value }}"
 {{- end }}
 GOOS="linux" go build -ldflags='-s -w' -o linux/amd64/bin/main "$GOMOD/cmd/main"
 GOOS="linux" GOARCH="arm64" go build -ldflags='-s -w' -o linux/arm64/bin/main "$GOMOD/cmd/main"
 
 if [ "${STRIP:-false}" != "false" ]; then
+  {{- range $key, $value := .}}
+  strip linux/amd64/{{ $key }} linux/arm64/{{ $key }}
+  {{- end }}
   strip linux/amd64/bin/main linux/arm64/bin/main
 fi
 
 if [ "${COMPRESS:-none}" != "none" ]; then
+  {{- range $key, $value := .}}
+  $COMPRESS linux/amd64/{{ $key }} linux/arm64/{{ $key }}
+  {{- end }}
   $COMPRESS linux/amd64/bin/main linux/arm64/bin/main
 fi
 
